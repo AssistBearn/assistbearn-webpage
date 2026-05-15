aa456c69835316231341089c3c82c4e97ce86ac2
AssistBearn <assistbearn@gmail.com>
2026-05-15 14:56:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Assist’béarn website loaded.");
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+window.addEventListener("scroll", () => {
+  document.querySelector("header").classList.toggle(
+    "scrolled",
+    window.scrollY > 50
+  );
+});
